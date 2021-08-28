6a5d2691dace12133b773493f6551a27dde7e037
GrishinAnton <grishin_rostov@mail.ru>
2021-08-28 21:38:04 +0300

    Добавил регистрация/авторизацию на сервере с защитой роута

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { CreateUserDto } from '../users/dto/create-user.dto';
 import { AuthService } from './auth.service';
+import { jwtAuthGuard } from './jwt-auth.guard';
 
 @ApiTags('Авторизация')
 @Controller('auth')
--- server/src/users/users.module.ts
+++ server/src/users/users.module.ts
@@ -1,13 +1,17 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { UserModel } from './user.model';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
   controllers: [UsersController],
   providers: [UsersService],
-  imports: [SequelizeModule.forFeature([UserModel])],
+  imports: [
+    SequelizeModule.forFeature([UserModel]),
+    forwardRef(() => AuthModule),
+  ],
   exports: [UsersService],
 })
 export class UsersModule {}
